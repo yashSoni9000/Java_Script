42150379e2a5107e2f7c1663ca7e46a717f85f1d
Yash <ys77357@gmail.com>
2022-10-30 16:52:56 +0530

    First Commit

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,15 @@
+let country ="India";
+let continent="Asis";
+let population =1417;
+
+console.log(country);
+console.log(continent);
+console.log(population+" million");
+
+let isIsland=false;
+let language;
+
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,6 @@
+let a="Hello world";
+
+// if(a==="Hello world") alert("Yayyy!!");
+
+let isProfession="Coder";
+console.log(69+69);
