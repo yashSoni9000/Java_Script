c2d9cb86535cfe6b6a1641df4bde71395ec8e1d1
Yash <ys77357@gmail.com>
2022-10-31 01:57:06 +0530

    updated assignment and script files

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,15 +1,17 @@
-let country ="India";
-let continent="Asis";
+const country ="India";
+const continent="Asia";
 let population =1417;
 
+// country="Findland"; this throws an error as const can not be changed later
 console.log(country);
 console.log(continent);
 console.log(population+" million");
 
-let isIsland=false;
-let language;
+const isIsland=false;
+const language="Hindi";
 
 console.log(typeof country);
 console.log(typeof population);
 console.log(typeof isIsland);
 console.log(typeof language);
+
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,25 @@
-let a="Hello world";
+// let a="Hello world";
 
 // if(a==="Hello world") alert("Yayyy!!");
 
-let isProfession="Coder";
-console.log(69+69);
+// let isProfession="Coder";
+// console.log(69+69);
+
+
+//the are 3 types of variables let ,const and var
+//let allows us to change the values stored in it at a later point of time
+let age=20;
+age=21;
+console.log(age);
+
+//but const type dosent allow the values to be changed in the program
+const birthyear=2002;
+//birthyear=2003;//error thrown
+//also empty const variables are also not legal as this dosent make sense to undefine a constant
+
+// const and let are modern js functions i.e. ES6
+
+//one thing to note is that we should use const as much as possible because mutation(changing values) may generated bugs
+//use let only if you are sure that you need to change the values in that variable
+
+//we can declare a variable without using let, const or var but its a terrible idea as it declares them globally
