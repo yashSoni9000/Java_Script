49c9ac7e71baacc9a223b7cff568122d7cb637a0
Yash <ys77357@gmail.com>
2022-11-20 16:41:13 +0530

    updated script file

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -15,22 +15,31 @@
 
 // checkWinner(avgDolphins,avgKoalas);
 
-//coding challenge 2
+// //coding challenge 2
 
-const calcTip = (bill) => {
-  return bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
-};
+// const calcTip = (bill) => {
+//   return bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+// };
 
-// const ans = calcTip(100);
-// console.log(ans);
+// // const ans = calcTip(100);
+// // console.log(ans);
 
-const bills = [125, 555, 44];
-const tips = [0];
-tips[0] = calcTip(bills[0]);
-tips[1] = calcTip(bills[1]);
-tips[2] = calcTip(bills[2]);
-const total = [0];
-total[0] = tips[0] + bills[0];
-total[1] = tips[1] + bills[1];
-total[2] = tips[2] + bills[2];
-console.log(total);
+// const bills = [125, 555, 44];
+// const tips = [0];
+// tips[0] = calcTip(bills[0]);
+// tips[1] = calcTip(bills[1]);
+// tips[2] = calcTip(bills[2]);
+// const total = [0];
+// total[0] = tips[0] + bills[0];
+// total[1] = tips[1] + bills[1];
+// total[2] = tips[2] + bills[2];
+// console.log(total);
+
+// const yash = {
+//   name: "yash soni",
+//   noOfFriends: 3,
+//   bestFriend: "Myself",
+// };
+// console.log(
+//   `${yash.name} has ${yash.noOfFriends}, and his best friend is ${yash.bestFriend}`
+// );
