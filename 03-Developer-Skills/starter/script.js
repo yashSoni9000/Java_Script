ac5509230fe4d772f2db7530a23841907c499922
Yash <ys77357@gmail.com>
2022-12-01 09:29:19 +0530

    completed challenge of dev skills

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const printForecast = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${arr[i]} degree Celsius in day ${i + 1}`);
+  }
+};
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+printForecast(data1);
+printForecast(data2);
