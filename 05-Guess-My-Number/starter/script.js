5315029be97926aa393562125bff0885899384f2
Yash <ys77357@gmail.com>
2022-12-14 15:09:30 +0530

    Added more functionalities to game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,58 @@
 'use strict';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Hello';
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = '69';
+// console.log((document.querySelector('.number').textContent = '69'));
+
+// // console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 30;
+// console.log(document.querySelector('.guess').value);
+
+// document.querySelector('.number');
+// const result = right.querySelector('.message').textContent;
+// console.log(result);
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = Number(document.querySelector('.score').textContent);
+// document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  // console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Input!!';
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low..';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost :(';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High..';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost :(';
+      document.querySelector('.score').value = 0;
+    }
+  } else if (guess === secretNumber) {
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.message').textContent = '🏆You Win!!';
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+});
