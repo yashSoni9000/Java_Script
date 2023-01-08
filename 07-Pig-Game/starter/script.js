137efd9e1b0691056b8b8c18e97ba9f8d8e602ca
Yash <ys77357@gmail.com>
2023-01-08 18:38:58 +0530

    Added functionality of holding game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -56,10 +56,58 @@
 //   //   }
 // };
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const score = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    document.getElementById(`score--${activePlayer}`).textContent = 0;
+    activePlayer = Number(!activePlayer);
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+    // document.getElementById(
+    //   `current--${Number(!activePlayer)}`
+    // ).textContent = 0;
+  }
+});
+
+const activeScore = document.getElementById(
+  `score--${activePlayer}`
+).textContent;
+
+btnHold.addEventListener('click', function () {
+  document.getElementById(`score--${activePlayer}`).textContent =
+    Number(document.getElementById(`score--${activePlayer}`).textContent) +
+    currentScore;
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = Number(!activePlayer);
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+  // if(Number(activeScore)>=100)
+});
