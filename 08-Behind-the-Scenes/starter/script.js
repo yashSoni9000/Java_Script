112bae9fad60eaabd1280e4f516743f73a906793
Yash <ys77357@gmail.com>
2023-01-10 01:24:22 +0530

    testing with different types of function

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+console.log(addNum(2, 3));
+console.log(undefined(2, 3));
+// console.log(undefined(2, 3));
+
+function addNum(a, b) {
+  return a + b;
+}
+const undefined = function (a, b) {
+  return a + b;
+};
+// var undefined = function (a, b) {
+//   return a + b;
+// };
