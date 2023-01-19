dbf575ab35aca566a8b580b833e8ab21ecaba697
Yash <ys77357@gmail.com>
2023-01-20 01:25:58 +0530

    Completed challenge #3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -367,3 +367,37 @@ for (const [key, value] of question) {
 
 // to convert maps to array
 console.log([...question]);
+
+// coding challenge #3
+console.log('Coding Challenge #3');
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '� Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '� Substitution'],
+  [64, '� Yellow card'],
+  [69, '� Red card'],
+  [70, '� Substitution'],
+  [72, '� Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '� Yellow card'],
+]);
+
+const events = new Set([...gameEvents.values()]);
+console.log(...events);
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+for (const [key, value] of gameEvents) {
+  console.log(
+    key <= 45
+      ? `[FIRST HALF] ${key}: ${value}`
+      : `[SECOND HALF] ${key}: ${value}`
+  );
+}
