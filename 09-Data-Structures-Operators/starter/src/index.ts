ca48f9dd9d9f3f4320b44ede20110d056d17540e
Yash <ys77357@gmail.com>
2023-01-22 01:04:44 +0530

    Co-authored-by: Deepesh Choudhary <deepesh-bex@users.noreply.github.com>

--- 09-Data-Structures-Operators/starter/src/index.ts
+++ 09-Data-Structures-Operators/starter/src/index.ts
@@ -1,7 +1,5 @@
-'use strict';
-
 // Data needed for a later exercise
-const flights =
+const flights: string =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
@@ -52,16 +50,16 @@ const flights =
 
 // OR
 
-function getCode(str) {
-  return str.slice(0, 3).toUpperCase();
-}
+const getCode = (str: string): string => str.slice(0, 3).toUpperCase();
 
 for (const flight of flights.split('+')) {
-  const [type, from, to, time] = flight.split(';');
-  const output = `${type.startsWith('_Delayed') ? '🛑' : ''}${type.replaceAll(
-    '_',
-    ' '
-  )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(35);
+  const [type, from, to, time]: string[] = flight.split(';');
+  const output: string = `${
+    type.startsWith('_Delayed') ? '🛑' : ''
+  }${type.replaceAll('_', ' ')} ${getCode(from)} ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(35);
   console.log(output);
 }
 
