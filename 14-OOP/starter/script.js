74c5a7501b395ff2c819fb929faa26823fa3cb0a
Yash <ys77357@gmail.com>
2023-02-01 01:21:51 +0530

    Paused Oops

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// arrow function does not work in constructor as arrow does not have its own this function
+const Person = function (firstName, birthYear) {
+  //   console.log(this);
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  // do not use methods in object as in large code there will be many copies of it
+  //   this.calcAge=function(){
+  //     console.log(2037-this.birthYear);
+  //   }
+};
+const yash = new Person('Yash', 2002);
+// console.log(yash);
+//new empty object is created when we use the 'new' keyword
+const hemu = new Person('Hemlata', 1980);
+// console.log(hemu);
+// console.log(yash instanceof Person);
+
+//Prototypes
+
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+yash.calcAge();
