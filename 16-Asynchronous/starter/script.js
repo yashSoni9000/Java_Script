9bb2d10b1a5357cdd247c1a5851938710bb3efab
Yash <ys77357@gmail.com>
2023-02-02 00:59:24 +0530

    Learnt callback hell

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,108 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+/*
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  let i = 0;
+  if (country === 'india') i = 1;
+  request.send();
+  //   console.log(this.responseText);
+
+  request.addEventListener('load', function () {
+    //   console.log(this.responseText);
+    //   const data = JSON.parse(this.responseText);
+    // we destructue it to get the object in the array
+    const data = JSON.parse(this.responseText)[i];
+    console.log(data);
+    const html = `<article class="country">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+</article>`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('india');
+getCountryData('usa');
+*/
+
+// when a callback calls another callback and that callback also calls anothe one then that
+// is called a callback hell aka nested callbacks
+// to identify a callback hell we starts to see more indentation like a pyramind (e.g. line 100)
+
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+const getCountryAndNeighbour = function (country) {
+  //AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  let i = 0;
+  if (country === 'india') i = 1;
+  request.send();
+  //   console.log(this.responseText);
+
+  request.addEventListener('load', function () {
+    //   console.log(this.responseText);
+    //   const data = JSON.parse(this.responseText);
+    // we destructue it to get the object in the array
+    const data = JSON.parse(this.responseText)[i];
+    console.log(data);
+    //rendering country 1
+    renderCountry(data);
+    //neighbour country
+    const neighbour = data.borders?.[0];
+    // console.log(neighbour);
+    if (!neighbour) return;
+    //AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+    request2.addEventListener('load', function () {
+      //   console.log(this.responseText);
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// getCountryAndNeighbour('india');
+getCountryAndNeighbour('usa');
+
+setTimeout(() => {
+  console.log('1 Second Passed');
+  setTimeout(() => {
+    console.log('2 Second Passed');
+    setTimeout(() => {
+      console.log('3 Second Passed');
+      setTimeout(() => {
+        console.log('4 Second Passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
